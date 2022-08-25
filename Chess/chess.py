30c9f8db43b7a452a10d4282a9fbc3924737f2d7
Adesh-04 <adeshgadekar98@gmail.com>
2022-08-25 23:59:19 +0530

    pawn Movement

--- Chess/chess.py
+++ Chess/chess.py
@@ -15,10 +15,18 @@ class Chess:
     def __init__(self):
         self.rank = 8  ## [1,2,3,4,5,6,7,8]
         self.file = 8
-        self.file_notation = ['0','H','G','F','E','D','C','B','A']
+        self.file_notation = ['0','h','g','f','e','d','c','b','a']
         self.board = self.createBoard()
         self.side = 'White'
         self.move_count = 0
+
+    def change_side(self):
+        if self.side == 'White':
+            self.side = 'Black'
+            self.move_count+=1
+        elif self.side == 'Black':
+            self.side = 'White'
+            self.move_count+=1
 
     def createBoard(self):
         ## Ranks are horizontal and Files are Vertical
@@ -48,25 +56,6 @@ class Chess:
         self.board[1][4],self.board[1][5] = 'WK','WQ'
         self.board[8][4],self.board[8][5] = 'BK','BQ'
 
-    def take_input(self):
-        var = input("{}'s Move :".format(self.side))
-        var = list(var)
-        return self.move(var)
-
-    def move(self,arr):
-        if arr:
-            ## E5 have [file][rank]
-            if len(arr) == 2:
-                if arr[0] in self.file_notation and int(arr[1]) < self.rank:
-                    self.move_pawn(arr)
-
-    def move_pawn(self,arr):
-        rank_index = int(arr[1]) 
-        file_index = self.file_notation.index(arr[0])
-        print(self.board[rank_index][file_index])  ## Getting Location
-        
-        
-
     def display(self):
         print('\n\t   *** Black Side ***')
         for i in range(8,0,-1):
@@ -77,12 +66,53 @@ class Chess:
         print('\n\n  A{s}B{s}C{s}D{s}E{s}F{s}G{s}H  '.format(s='     '))
         print('\n\t   *** White Side ***\n')
 
+    def take_input(self):
+        var = input("{}'s Move :".format(self.side))
+        var = list(var)
+        return self.move(var)
 
+    def move(self,arr):
+        if arr:
+            ## arr eg. E5 have [file][rank]
+            if len(arr) == 2:
+                if arr[0] in self.file_notation and int(arr[1]) < self.rank:
+                    self.move_pawn(arr)
+                else: 
+                    print('Invalid Location for 2') 
+            elif len(arr) == 3:
+                if 'x' in arr:
+                    print('yes')
+            else:
+                return -1
+        
+    def move_pawn(self,arr):
+        r = int(arr[1])  ## rank_index
+        f = self.file_notation.index(arr[0]) ## file_index
+
+        if self.board[r][f] == '00':
+            if self.side == 'White' and self.board[r-1][f] == 'WP':
+                self.board[r][f] = self.board[r-1][f]
+                self.board[r-1][f] = '00'
+                self.change_side()
+
+            elif self.side == 'Black' and self.board[r+1][f] == 'BP':
+                self.board[r][f] = self.board[r+1][f]
+                self.board[r+1][f] = '00'
+                self.change_side()
+
+            else: print('Move error')
+        else: print('Position is taken')
+        
+
+  
 if __name__ == '__main__':
 
     obj = Chess()
     obj.putPieces()
-    obj.display()
-    obj.take_input()
+    while True:
+        obj.display()
+        ch = obj.take_input()
+        if ch == -1:
+            break
 
 
